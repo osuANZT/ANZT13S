0361663abfd1277da09971c393da7864fe9c3a89
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2026-01-05 22:51:48 +1100

    Complete beatmap updating

--- showcase/index.js
+++ showcase/index.js
@@ -20,6 +20,17 @@ const nowPlayingStatLenEl = document.getElementById("now-playing-stat-len")
 const nowPlayingStatBpmEl = document.getElementById("now-playing-stat-bpm")
 const nowPlayingStatCirclesEl = document.getElementById("now-playing-stat-circles")
 const nowPlayingStatSlidersEl = document.getElementById("now-playing-stat-sliders")
+// Now Playing Replayer Details
+const nowPlayingReplayerIconEl = document.getElementById("now-playing-replayer-icon")
+const nowPlayingReplayerNameEl = document.getElementById("now-playing-replayer-name")
+let currentReplayerName
+// Now Playing Metadata
+const vinylMapEl = document.getElementById("vinyl-map")
+const nowPlayingMetadataArtistEl = document.getElementById("now-playing-metadata-artist")
+const nowPlayingMetadataTitleEl = document.getElementById("now-playing-metadata-title")
+const nowPlayingMetadataDifficultyEl = document.getElementById("now-playing-metadata-difficulty")
+const nowPlayingMetadataMapperEl = document.getElementById("now-playing-metadata-mapper")
+let currentMapId, currentMapChecksum
 
 // Socket
 const socket = createTosuWsSocket()
@@ -28,14 +39,34 @@ socket.onmessage = event => {
     console.log(data)
 
     // Stats
-    nowPlayingStatCsEl.textContent = data.beatmap.stats.cs.converted.toFixed(1)
-    nowPlayingStatArEl.textContent = data.beatmap.stats.ar.converted.toFixed(1)
-    nowPlayingStatOdEl.textContent = data.beatmap.stats.od.converted.toFixed(1)
-    nowPlayingStatSrEl.textContent = data.beatmap.stats.stars.total.toFixed(2)
-    nowPlayingStatLenEl.textContent = setLengthDisplay(Math.round((data.beatmap.time.lastObject - data.beatmap.time.firstObject) / 1000))
-    nowPlayingStatBpmEl.textContent = data.beatmap.stats.bpm.common
-    nowPlayingStatCirclesEl.textContent = data.beatmap.stats.objects.circles
-    nowPlayingStatSlidersEl.textContent = data.beatmap.stats.objects.sliders
-
+    const beatmapData = data.beatmap
+    const beatmapDataStats = beatmapData.stats
+    nowPlayingStatCsEl.textContent = beatmapDataStats.cs.converted.toFixed(1)
+    nowPlayingStatArEl.textContent = beatmapDataStats.ar.converted.toFixed(1)
+    nowPlayingStatOdEl.textContent = beatmapDataStats.od.converted.toFixed(1)
+    nowPlayingStatSrEl.textContent = beatmapDataStats.stars.total.toFixed(2)
     // TODO: For Length, get original BPM of the map, then current BPM. then do conversion for length
+    nowPlayingStatLenEl.textContent = setLengthDisplay(Math.round((beatmapData.time.lastObject - beatmapData.time.firstObject) / 1000))
+    nowPlayingStatBpmEl.textContent = beatmapDataStats.bpm.common
+
+    if (currentReplayerName !== data.resultsScreen.playerName) {
+        // Replayer Details
+        currentReplayerName = data.resultsScreen.playerName
+        nowPlayingReplayerIconEl.setAttribute("src", `https://a.ppy.sh/${2}`)
+        nowPlayingReplayerNameEl.textContent = currentReplayerName
+    }
+
+    if (currentMapId !== beatmapData.id || currentMapChecksum !== beatmapData.checksum) {
+        // Objects
+        const beatmapDataStatsObjects = beatmapDataStats.objects
+        nowPlayingStatCirclesEl.textContent = beatmapDataStatsObjects.circles
+        nowPlayingStatSlidersEl.textContent = beatmapDataStatsObjects.sliders
+
+        // Metadata
+        vinylMapEl.style.backgroundImage = `url("https://assets.ppy.sh/beatmaps/${beatmapData.set}/covers/cover.jpg")`
+        nowPlayingMetadataArtistEl.textContent = beatmapData.artist
+        nowPlayingMetadataTitleEl.textContent = beatmapData.title
+        nowPlayingMetadataDifficultyEl.textContent = beatmapData.version
+        nowPlayingMetadataMapperEl.textContent = beatmapData.mapper   
+    }
 }
