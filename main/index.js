45f6c25f9c8b16f85b4193e597b48ba02c577ec0
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2026-02-05 14:59:19 +1100

    feat: complete update chat on gameplay scene

--- main/index.js
+++ main/index.js
@@ -1,11 +1,14 @@
+import { getLogsApi } from "../_shared/core/apis.js"
 import { findBeatmap, loadBeatmaps } from "../_shared/core/beatmaps.js"
+import { updateChat } from "../_shared/core/chat.js"
 import { toggleStarContainers, renderStars } from "../_shared/core/stars.js"
+import { loadTeams, findTeam } from "../_shared/core/teams.js"
 import { getCookie } from "../_shared/core/utils.js"
 import { createTosuWsSocket } from "../_shared/core/websocket.js"
 
 const roundNameEl = document.getElementById("round-name")
 let allBeatmaps = []
-Promise.all([loadBeatmaps()]).then(([beatmaps]) => {
+Promise.all([loadBeatmaps(), loadTeams]).then(([beatmaps, teams]) => {
     // Load beatmaps
     allBeatmaps = beatmaps.beatmaps
     roundNameEl.textContent = `// ${beatmaps.roundName} gameplay`
@@ -37,7 +40,7 @@ let noOfClients, previousNoOfClients
 
 // Scores
 const scoreLeftEl = document.getElementById("score-left")
-const scoreRightEl = document.getElementById("score-left")
+const scoreRightEl = document.getElementById("score-right")
 // Score Difference
 const scoreDifferenceLeftEl = document.getElementById("score-difference-left")
 const scoreDifferenceRightEl = document.getElementById("score-difference-right")
@@ -54,9 +57,13 @@ const animation = {
     // Score
     "scoreLeft": new CountUp(scoreLeftEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ",", decimal: ".", suffix: ""}),
     "scoreRight": new CountUp(scoreRightEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ",", decimal: ".", suffix: ""}),
-    "scoreDifferenceLeft": new CountUp(scoreLeftEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ",", decimal: ".", suffix: ""}),
-    "scoreDifferenceRight": new CountUp(scoreRightEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ",", decimal: ".", suffix: ""}),
+    "scoreDifferenceLeft": new CountUp(scoreDifferenceLeftEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ",", decimal: ".", suffix: ""}),
+    "scoreDifferenceRight": new CountUp(scoreDifferenceRightEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ",", decimal: ".", suffix: ""}),
 }
+
+// Chat variables
+const chatDisplayWrapperEl = document.getElementById("chat-display-wrapper")
+let chatLen
 
 // Socket
 const socket = createTosuWsSocket()
@@ -143,9 +150,9 @@ socket.onmessage = event => {
         animation.scoreRight.update(currentBlueScore)
 
         // Show score difference
-        const scoreDifference = -Math.abs(currentRedScore - currentBlueScore)
-        animation.scoreDifferenceLeft.update(scoreDifference)
-        animation.scoreDifferenceRight.update(scoreDifference)
+        const scoreDifference = Math.abs(currentRedScore - currentBlueScore)
+        animation.scoreDifferenceLeft.update(-scoreDifference)
+        animation.scoreDifferenceRight.update(-scoreDifference)
 
         // Score bar width
         let multiplier = 0.5
@@ -203,6 +210,11 @@ socket.onmessage = event => {
             winnerBlueCrownEl.style.opacity =crownOpacity
         }
     }
+
+    // This is also mostly taken from Victim Crasher: https://github.com/VictimCrasher/static/tree/master/WaveTournament
+    if (chatLen !== data.tourney.chat.length) {
+        chatLen = updateChat(data.tourney, chatLen, chatDisplayWrapperEl, false, getLogsApi(), currentTeamRed, currentTeamBlue, currentTeamRedName, currentTeamBlueName)
+    }
 }
 
 // Team Name Elements
@@ -214,6 +226,7 @@ const redTeamStarContainerEl = document.getElementById("red-team-star-container"
 const blueTeamStarContainerEl = document.getElementById("blue-team-star-container")
 
 let currentTeamRedName, currentTeamBlueName, previousTeamRedName, previousTeamBlueName
+let currentTeamRed, currentTeamBlue
 setInterval(() => {
     // Set team name information
     currentTeamRedName = getCookie("currentTeamRedName")
@@ -222,10 +235,12 @@ setInterval(() => {
     if (currentTeamRedName !== previousTeamRedName) {
         previousTeamRedName = currentTeamRedName
         teamRedNameEl.textContent = currentTeamRedName
+        currentTeamRed = findTeam(currentTeamRedName)
     }
     if (currentTeamBlueName !== previousTeamBlueName) {
         previousTeamBlueName = currentTeamBlueName
         teamBlueNameEl.textContent = currentTeamBlueName
+        currentTeamBlue = findTeam(currentTeamRedName)
     }
 
     // Toggle and render stars
